5abf2a633d549b8a813f8820452e104431fc1f23
hgiang0820 <nhgiang0820@gmail.com>
2024-12-08 19:55:24 +0700

    init project

---
+++ UrlShortenerService/Controllers/UrlsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UrlShortenerService.Data;
+using UrlShortenerService.Models;
+
+namespace UrlShortenerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlsController : ControllerBase
+    {
+        private readonly UrlShortenerServiceContext _context;
+
+        public UrlsController(UrlShortenerServiceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Urls
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Url>>> GetUrl()
+        {
+            return await _context.Url.ToListAsync();
+        }
+
+        // GET: api/Urls/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Url>> GetUrl(int id)
+        {
+            var url = await _context.Url.FindAsync(id);
+
+            if (url == null)
+            {
+                return NotFound();
+            }
+
+            return url;
+        }
+
+        // PUT: api/Urls/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUrl(int id, Url url)
+        {
+            if (id != url.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(url).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UrlExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Urls
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Url>> PostUrl(Url url)
+        {
+            var new_url = new Url()
+            {
+                Id = url.Id,
+                OriginalUrl = url.OriginalUrl,
+                ShortCode =  GenerateShortCode(),
+                CreatedAt = DateTime.Now,
+            };
+
+            _context.Url.Add(new_url);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUrl", new { id = url.Id }, url);
+        }
+
+        private string GenerateShortCode()
+        {
+            return Guid.NewGuid().ToString().Substring(0, 6);
+        }
+
+        // DELETE: api/Urls/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUrl(int id)
+        {
+            var url = await _context.Url.FindAsync(id);
+            if (url == null)
+            {
+                return NotFound();
+            }
+
+            _context.Url.Remove(url);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UrlExists(int id)
+        {
+            return _context.Url.Any(e => e.Id == id);
+        }
+    }
+}
